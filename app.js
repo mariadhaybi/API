1d1c0d1c696710036fb1ac1c8ffe21e2d3650871
unknown <maria@gmail.com>
2024-08-01 13:14:34 +0300

    reading data from excel file

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 //require('dotenv').config();
+const fs =require('fs');
+const xlsx = require('xlsx');  
+let wb = xlsx.readFile('data.xlsx');
+let ws = wb.Sheets['names'];
+let _data = xlsx.utils.sheet_to_json(ws);
+
 const PORT = process.env.PORT || 3030;
 
 // your code
@@ -8,14 +14,13 @@ app.get('/', function (req, res) {
     res.send('Hello World')})
     
     app.get('/students', function (req, res) {
-        var data = [
-            {id:1,name:'maria dh'},
-            {id:6,name:'maria 1'},
-            {id:9,name:'maria 2'},
-            {id:5,name:'maria 3'},
-
-        ]
-        res.send(data)})
+       // var data = [
+            //{id:1,name:'maria dh'},
+           // {id:6,name:'maria 1'},
+           // {id:9,name:'maria 2'},
+            //{id:5,name:'maria 3'},
+       // ]
+        res.send(_data)})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
