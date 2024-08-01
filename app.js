197ce32e6bfaf2327d4bd80f35e8c72f74fb706a
unknown <maria@gmail.com>
2024-08-01 12:32:07 +0300

    adding new Endpoint

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
+app.get('/', function (req, res) {
+    res.send('Hello World')})
+    
+    app.get('/students', function (req, res) {
+        var data = [
+            {id:1,name:'maria dh'},
+            {id:6,name:'maria 1'},
+            {id:9,name:'maria 2'},
+            {id:5,name:'maria 3'},
+
+        ]
+        res.send(data)})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
